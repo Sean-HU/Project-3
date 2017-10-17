6d4268c51085e628fd9a58f0587051cdd71900a6
Sean-HU <32807155+Sean-HU@users.noreply.github.com>
2017-10-17 00:25:57 -0400

    Add files via upload

--- App Dev Project 3/javalib.js
+++ App Dev Project 3/javalib.js
@@ -23,3 +23,89 @@ function coursesTimeout() {
 function projectsTimeout() {
 	window.open("hobby.html","_self")
 }
+
+function hobbyTimeout() {
+	window.open("friends.html","_self")
+}
+
+function friendsTimeout() {
+	window.open("home.html","_self")
+}
+
+function keyPressedHome(event) {
+	var key = event.keyCode;
+	if ((key == 38) || (key == 37)) {
+		window.open("friends.html","_self")
+	}
+	else if ((key == 39) || (key == 40)) {
+		window.open("courses.html","_self")
+	}
+	else {
+		window.clearTimeout(timeoutReset);
+		timeoutReset = window.setTimeout(homeTimeout, 30000);
+	}
+}
+
+function keyPressedCourses(event) {
+	var key = event.keyCode;
+	if ((key == 38) || (key == 37)) {
+		window.open("home.html","_self")
+	}
+	else if ((key == 39) || (key == 40)) {
+		window.open("projects.html","_self")
+	}
+	else {
+		window.clearTimeout(timeoutReset);
+		timeoutReset = window.setTimeout(coursesTimeout, 30000);
+	}
+}
+
+function keyPressedProjects(event) {
+	var key = event.keyCode;
+	if ((key == 38) || (key == 37)) {
+		window.open("courses.html","_self")
+	}
+	else if ((key == 39) || (key == 40)) {
+		window.open("hobby.html","_self")
+	}
+	else {
+		window.clearTimeout(timeoutReset);
+		timeoutReset = window.setTimeout(projectsTimeout, 30000);
+	}
+}
+
+function keyPressedHobby(event) {
+	var key = event.keyCode;
+	if ((key == 38) || (key == 37)) {
+		window.open("projects.html","_self")
+	}
+	else if ((key == 39) || (key == 40)) {
+		window.open("friends.html","_self")
+	}
+	else {
+		window.clearTimeout(timeoutReset);
+		timeoutReset = window.setTimeout(hobbyTimeout, 30000);
+	}
+}
+
+function keyPressedFriends(event) {
+	var key = event.keyCode;
+	if ((key == 38) || (key == 37)) {
+		window.open("hobby.html","_self")
+	}
+	else if ((key == 39) || (key == 40)) {
+		window.open("home.html","_self")
+	}
+	else {
+		window.clearTimeout(timeoutReset);
+		timeoutReset = window.setTimeout(friendsTimeout, 30000);
+	}
+}
+
+function myFunction() {
+    if (document.body.scrollTop >= 0 || document.documentElement.scrollTop >= 0) {
+        window.open("courses.html","_self")
+    } else {
+        window.open("friends.html","_self")
+    }
+}
